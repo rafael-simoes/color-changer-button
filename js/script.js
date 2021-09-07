9a25e0a97cb6cf18fc726ed823d74e5408548137
rafael-simoes <blazerafa@gmail.com>
2021-09-07 15:45:44 +0100

    added custom and random color

--- js/script.js
+++ js/script.js
@@ -10,3 +10,17 @@ function buttonClick() {
         document.body.style.backgroundColor = `#${this.getAttribute('data-background')}`;
     }
 }
+const colorTxt = document.getElementById('colortext');
+
+function textchange() {
+    document.body.style.backgroundColor = `#${colorTxt.value}`;
+}
+
+
+
+randomBtn.addEventListener('click', RandomClick);
+
+function RandomClick(){var randomColor = Math.floor(Math.random()*16777215).toString(16);
+    document.body.style.backgroundColor = `#${randomColor}`;
+    document.getElementById('colorHex').innerHTML = '#'+randomColor;
+}
